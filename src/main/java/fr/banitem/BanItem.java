da1821c6538f3afd7e22d42540f098135731ee6f
André <sustac.andre@gmail.com>
2019-05-29 17:22:31 +0300

    Update v1.2

--- src/main/java/fr/banitem/BanItem.java
+++ src/main/java/fr/banitem/BanItem.java
@@ -6,22 +6,19 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDropItemEvent;
-import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
 
-public class BanItem extends JavaPlugin implements Listener {
+public class BanItem extends JavaPlugin {
     private final Map<String, Map<Material, BannedItem>> banned = new HashMap<>();
 
     @Override
@@ -35,6 +32,20 @@ public class BanItem extends JavaPlugin implements Listener {
         saveDefaultConfig();
         reloadConfig();
 
+        // Checking versions
+        boolean v12OrMore = getServer().getBukkitVersion().matches("1\\.12|1\\.13|1\\.14");
+        boolean v11OrMore = getServer().getBukkitVersion().matches("1\\.11|1\\.12|1\\.13|1\\.14");
+
+        // Preparing listeners
+        HandlerList.unregisterAll(this);
+        Listener listener = new Listener(){};
+        EventPriority eventPriority = EventPriority.HIGHEST;
+        boolean blockPlaceEvent = false;
+        boolean blockBreakEvent = false;
+        boolean interactEvent = false;
+        boolean pickupItemEvent = false;
+        boolean dropItemEvent = false;
+
         // Loading banned items:
         banned.clear();
         for(String material : getConfig().getKeys(false)){
@@ -46,13 +57,12 @@ public class BanItem extends JavaPlugin implements Listener {
                 continue;
             }
 
-            boolean blockPlace = getConfig().getBoolean(material + ".block-place");
-            boolean blockBreak = getConfig().getBoolean(material + ".block-break");
-            boolean pickUp = getConfig().getBoolean(material + ".pickup");
-            boolean drop = getConfig().getBoolean(material + ".drop");
-            boolean interact = getConfig().getBoolean(material + ".interact");
-            String message = getConfig().getString(material + ".message");
-            final BannedItem bi = new BannedItem(blockPlace, blockBreak, pickUp, drop, interact, message);
+            final BannedItem bi = new BannedItem(getConfig().getBoolean(material + ".block-place"),
+                    getConfig().getBoolean(material + ".block-break"),
+                    getConfig().getBoolean(material + ".pickup"),
+                    getConfig().getBoolean(material + ".drop"),
+                    getConfig().getBoolean(material + ".interact"),
+                    getConfig().getString(material + ".message"));
 
 
             List<String> worlds = new ArrayList<>();
@@ -70,11 +80,41 @@ public class BanItem extends JavaPlugin implements Listener {
                     addBannedItem(w.getName(), m, bi);
                 }
             }
-        }
 
-        // Registering listener
-        HandlerList.unregisterAll((JavaPlugin)this);
-        getServer().getPluginManager().registerEvents(this, this);
+            // Loading listeners
+            if(!blockPlaceEvent && !bi.canPlace()){
+                getServer().getPluginManager().registerEvent(BlockPlaceEvent.class, listener, eventPriority, (l, e) -> onBlockPlaceEvent((BlockPlaceEvent) e), this, true);
+                blockPlaceEvent = true;
+            }
+            if(!blockBreakEvent && !bi.canBreak()){
+                getServer().getPluginManager().registerEvent(BlockBreakEvent.class, listener, eventPriority, (l, e) -> onBlockBreakEvent((BlockBreakEvent) e), this, true);
+                blockBreakEvent = true;
+            }
+
+            if(!interactEvent && !bi.canPickUp()){
+                // >=1.11: It has EquipmentSlot
+                // <1.11: it doesn't
+                if(v11OrMore) getServer().getPluginManager().registerEvent(PlayerInteractEvent.class, listener, eventPriority, (l, e) -> onPlayerInteractEvent((org.bukkit.event.player.PlayerInteractEvent) e, true), this, true);
+                else getServer().getPluginManager().registerEvent(PlayerInteractEvent.class, listener, eventPriority, (l, e) -> onPlayerInteractEvent((org.bukkit.event.player.PlayerInteractEvent) e, false), this, true);
+                interactEvent = true;
+            }
+
+            if(!pickupItemEvent && !bi.canPickUp()){
+                // >=1.12: EntityPickupItemEvent
+                // <1.12: PlayerPickupItemEvent
+                if(v12OrMore) getServer().getPluginManager().registerEvent(org.bukkit.event.entity.EntityPickupItemEvent.class, listener, eventPriority, (l, e) -> onEntityPickupItemEvent((org.bukkit.event.entity.EntityPickupItemEvent) e), this, true);
+                else getServer().getPluginManager().registerEvent(org.bukkit.event.player.PlayerPickupItemEvent.class, listener, eventPriority, (l, e) -> onPlayerPickupItemEvent((org.bukkit.event.player.PlayerPickupItemEvent) e), this, true);
+                pickupItemEvent = true;
+            }
+
+            if(!dropItemEvent && !bi.canDrop()){
+                // >=1.12: EntityDropItemEvent
+                // <1.12: PlayerDropItemEvent
+                if(v12OrMore) getServer().getPluginManager().registerEvent(org.bukkit.event.entity.EntityDropItemEvent.class, listener, eventPriority, (l, e) -> onEntityDropItemEvent((org.bukkit.event.entity.EntityDropItemEvent) e), this, true);
+                else getServer().getPluginManager().registerEvent(org.bukkit.event.player.PlayerDropItemEvent.class, listener, eventPriority, (l, e) -> onPlayerDropItemEvent((org.bukkit.event.player.PlayerDropItemEvent) e), this, true);
+                dropItemEvent = true;
+            }
+        }
     }
 
 
@@ -103,14 +143,12 @@ public class BanItem extends JavaPlugin implements Listener {
         return true;
     }
 
-
     private void addBannedItem(final String world, final Material m, final BannedItem bi){
         Map<Material, BannedItem> map = banned.get(world);
         if(map == null) map = new HashMap<>();
         map.put(m, bi);
         banned.put(world, map);
     }
-
 
     private BannedItem getBannedItem(final Player p, final Material m){
         final String wName = p.getWorld().getName();
@@ -123,47 +161,56 @@ public class BanItem extends JavaPlugin implements Listener {
     }
 
 
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockPlaceEvent(final BlockPlaceEvent e){
+    // Listeners
+    private void onBlockPlaceEvent(final BlockPlaceEvent e){
         BannedItem bi = getBannedItem(e.getPlayer(), e.getBlock().getType());
-        if(bi == null || bi.ignorePlaced()) return;
+        if(bi == null || bi.canPlace()) return;
         if(bi.getMessage() != null) e.getPlayer().sendMessage(color(bi.getMessage()));
         e.setCancelled(true);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockBreakEvent(final BlockBreakEvent e){
+    private void onBlockBreakEvent(final BlockBreakEvent e){
         BannedItem bi = getBannedItem(e.getPlayer(), e.getBlock().getType());
-        if(bi == null || bi.ignoreBreaked()) return;
+        if(bi == null || bi.canBreak()) return;
         if(bi.getMessage() != null) e.getPlayer().sendMessage(color(bi.getMessage()));
         e.setCancelled(true);
     }
 
+    private void onPlayerInteractEvent(final PlayerInteractEvent e, boolean equipment){
+        if(e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getClickedBlock() == null) return;
+        if(equipment && e.getHand() != EquipmentSlot.HAND) return;
+        BannedItem bi = getBannedItem(e.getPlayer(), e.getClickedBlock().getType());
+        if(bi == null || bi.canInteract()) return;
+        if(bi.getMessage() != null) e.getPlayer().sendMessage(color(bi.getMessage()));
+        e.setCancelled(true);
+    }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerPickupItemEvent(final EntityPickupItemEvent e){
+    private void onEntityPickupItemEvent(final org.bukkit.event.entity.EntityPickupItemEvent e){
         if(!(e.getEntity() instanceof Player)) return;
         BannedItem bi = getBannedItem((Player)e.getEntity(), e.getItem().getItemStack().getType());
-        if(bi == null || bi.ignorePickUp()) return;
+        if(bi == null || bi.canPickUp()) return;
         e.setCancelled(true);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerDropItem(final EntityDropItemEvent e){
+    private void onPlayerPickupItemEvent(final org.bukkit.event.player.PlayerPickupItemEvent e){
+        BannedItem bi = getBannedItem(e.getPlayer(), e.getItem().getItemStack().getType());
+        if(bi == null || bi.canPickUp()) return;
+        e.setCancelled(true);
+    }
+
+    private void onEntityDropItemEvent(final org.bukkit.event.entity.EntityDropItemEvent e){
         if(!(e.getEntity() instanceof Player)) return;
         BannedItem bi = getBannedItem((Player)e.getEntity(), e.getItemDrop().getItemStack().getType());
-        if(bi == null || bi.ignoreDrop()) return;
+        if(bi == null || bi.canDrop()) return;
         if(bi.getMessage() != null) e.getEntity().sendMessage(color(bi.getMessage()));
         e.setCancelled(true);
     }
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerInteractEvent(final PlayerInteractEvent e){
-        if(e.getAction() != Action.RIGHT_CLICK_BLOCK || e.getClickedBlock() == null || e.getHand() != EquipmentSlot.HAND) return;
-        BannedItem bi = getBannedItem(e.getPlayer(), e.getClickedBlock().getType());
-        if(bi == null || bi.ignoreInteract()) return;
+    private void onPlayerDropItemEvent(final org.bukkit.event.player.PlayerDropItemEvent e){
+        BannedItem bi = getBannedItem(e.getPlayer(), e.getItemDrop().getItemStack().getType());
+        if(bi == null || bi.canDrop()) return;
         if(bi.getMessage() != null) e.getPlayer().sendMessage(color(bi.getMessage()));
         e.setCancelled(true);
     }
+
 }
--- src/main/java/fr/banitem/BannedItem.java
+++ src/main/java/fr/banitem/BannedItem.java
@@ -9,18 +9,18 @@ final class BannedItem {
     private final String message;
 
     BannedItem(boolean blockPlace, boolean blockBreak, boolean pickUp, boolean drop, boolean interact, String message){
-        this.blockPlace = blockPlace;
-        this.blockBreak = blockBreak;
-        this.pickUp = pickUp;
-        this.drop = drop;
-        this.interact = interact;
+        this.blockPlace = !blockPlace;
+        this.blockBreak = !blockBreak;
+        this.pickUp = !pickUp;
+        this.drop = !drop;
+        this.interact = !interact;
         this.message = message;
     }
 
-    boolean ignorePlaced() { return !blockPlace; }
-    boolean ignoreBreaked() { return !blockBreak; }
-    boolean ignorePickUp() { return !pickUp; }
-    boolean ignoreDrop() { return !drop; }
-    boolean ignoreInteract(){ return !interact; }
+    boolean canPlace() { return blockPlace; }
+    boolean canBreak() { return blockBreak; }
+    boolean canPickUp() { return pickUp; }
+    boolean canDrop() { return drop; }
+    boolean canInteract(){ return interact; }
     String getMessage(){ return message; }
 }
