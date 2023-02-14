42b555253b07ef5627b2cedc34a66ed6ff104877
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2023-02-14 16:45:26 +0100

    Added bannable

--- BanItemPlugin/src/main/java/fr/andross/banitem/BanItem.java
+++ BanItemPlugin/src/main/java/fr/andross/banitem/BanItem.java
@@ -18,6 +18,7 @@
 package fr.andross.banitem;
 
 import fr.andross.banitem.commands.BanCommand;
+import fr.andross.banitem.logs.BanLog;
 import fr.andross.banitem.utils.Chat;
 import fr.andross.banitem.utils.metrics.Metrics;
 import org.bukkit.Bukkit;
@@ -44,6 +45,7 @@ public final class BanItem extends JavaPlugin {
     private static BanItem instance;
     private BanItemAPI api;
     private BanConfig banConfig;
+    private BanLog banLog = new BanLog(this);
     private BanHooks hooks;
     private BanDatabase banDatabase;
     private final BanUtils utils = new BanUtils(this);
@@ -210,6 +212,10 @@ public final class BanItem extends JavaPlugin {
     @NotNull
     public BanConfig getBanConfig() {
         return banConfig;
+    }
+
+    public BanLog getBanLog() {
+        return banLog;
     }
 
     /**
