42c0cbbc0235b7e6d97a15aa3144556b5025c190
Conorsmine <79007923+Conorsmine@users.noreply.github.com>
2023-02-15 21:22:20 +0100

    Support for arrays
    Added the support for any array and cleaned up the code a bit

--- BanItemPlugin/src/main/java/fr/andross/banitem/items/meta/NBTAPI.java
+++ BanItemPlugin/src/main/java/fr/andross/banitem/items/meta/NBTAPI.java
@@ -19,6 +19,7 @@ package fr.andross.banitem.items.meta;
 
 import de.tr7zw.nbtapi.NBTCompound;
 import de.tr7zw.nbtapi.NBTItem;
+import de.tr7zw.nbtapi.NBTType;
 import fr.andross.banitem.utils.debug.Debug;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
@@ -27,7 +28,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
-import java.util.function.BiPredicate;
 
 /**
  * A simple meta comparator to compare NBT
@@ -35,7 +35,7 @@ import java.util.function.BiPredicate;
  * @author Andross
  */
 public final class NBTAPI extends MetaTypeComparator {
-    private final Map<List<String>, List<BiPredicate<NBTCompound, String>>> map = new HashMap<>();
+    private final Map<List<String>, List<Object>> map = new HashMap<>();
 
     public NBTAPI(final Object o, final Debug debug) {
         super(o);
@@ -63,7 +63,7 @@ public final class NBTAPI extends MetaTypeComparator {
 
             // Preparing variables
             final List<String> keys = Arrays.asList(keyNodes.split("#"));
-            final List<BiPredicate<NBTCompound, String>> predicates = new ArrayList<>();
+            final List<Object> expectedValues = new ArrayList<>();
 
             // Multiple matches
             if (object instanceof ConfigurationSection) {
@@ -71,77 +71,126 @@ public final class NBTAPI extends MetaTypeComparator {
                 // Getting all objects
                 for (final String objectName : keySection.getKeys(false)) {
                     final Object object2 = keySection.get(objectName);
-                    final BiPredicate<NBTCompound, String> predicate = getPredicate(object2);
-                    if (predicate == null) {
+                    if (object2 == null) {
                         debug.clone().add("&cUnknown object '" + objectName + "' for NBT node '" + keyNodes + "'.");
                         setValid(false);
                         return;
                     }
-                    predicates.add(predicate);
+                    expectedValues.add(object2);
                 }
-            } else { // Simple NBT
-                final BiPredicate<NBTCompound, String> predicate = getPredicate(object);
-                if (predicate == null) {
-                    debug.clone().add("&cUnknown object '" + object + "' for NBT node '" + keyNodes + "'.");
-                    setValid(false);
-                    return;
-                }
-                predicates.add(predicate);
             }
 
-            if (!predicates.isEmpty())
-                map.put(keys, predicates);
-        }
-    }
+            // Simple NBT
+            else
+                expectedValues.add(object);
 
-    @Nullable
-    private BiPredicate<NBTCompound, String> getPredicate(@Nullable final Object o) {
-        if (o instanceof String) return (c, k) -> o.equals(c.getString(k));
-        else if (o instanceof Boolean) return (c, k) -> o.equals(c.getBoolean(k));
-        else if (o instanceof Byte) return (c, k) -> o.equals(c.getByte(k));
-        else if (o instanceof Double) return (c, k) -> o.equals(c.getDouble(k));
-        else if (o instanceof Float) return (c, k) -> o.equals(c.getFloat(k));
-        else if (o instanceof Integer) return (c, k) -> o.equals(c.getInteger(k));
-        else if (o instanceof Long) return (c, k) -> o.equals(c.getLong(k));
-        else if (o instanceof Short) return (c, k) -> o.equals(c.getShort(k));
-        else if (o instanceof UUID) return (c, k) -> o.equals(c.getUUID(k));
-        else if (o instanceof List) return (c, k) -> o.equals(c.getStringList(k));
-        else if (o instanceof ItemStack) return (c, k) -> {
-            final ItemStack item = c.getItemStack(k);
-            return item != null && item.isSimilar((ItemStack) o);
-        };
-        else return null;
+
+            if (!expectedValues.isEmpty())
+                map.put(keys, expectedValues);
+        }
     }
 
     @Override
     public boolean matches(@NotNull final ItemStack itemStack, @Nullable final ItemMeta itemMeta) {
         final NBTItem nbtItem = new NBTItem(itemStack);
 
-        for (final Map.Entry<List<String>, List<BiPredicate<NBTCompound, String>>> e : map.entrySet()) {
-            final List<String> nodes = e.getKey();
-            String lastKey = null;
-            NBTCompound compound = nbtItem;
-
-            // Getting Compound
-            for (int i = 0; i < nodes.size(); i++) {
-                final String key = nodes.get(i);
-                // Last one?
-                if (i == nodes.size() - 1) {
-                    lastKey = key;
-                    break;
-                } else {
-                    if (!compound.hasKey(key)) return false;
-                    compound = compound.getCompound(key);
-                }
-            }
-
-            if (lastKey == null || compound == null) return false;
+        for (final Map.Entry<List<String>, List<Object>> e : map.entrySet()) {
+            NBTCompound compound = getNBTCompoundFromPath(e.getKey(), nbtItem);
+            if (compound == null) return false;
 
             // Matching object?
-            for (final BiPredicate<NBTCompound, String> predicate : e.getValue())
-                if (predicate.test(compound, lastKey))
-                    return true;
+            String lastKey = e.getKey().get(e.getKey().size() - 1);
+            if (!anyValueMatches(lastKey, e.getValue(), compound))
+                return false;
         }
+
+        return !map.entrySet().isEmpty();
+    }
+
+    private NBTCompound getNBTCompoundFromPath(List<String> keyList, final NBTCompound nbtItem) {
+        NBTCompound compound = nbtItem;
+
+        Iterator<String> keyIterator = keyList.iterator();
+        while (keyIterator.hasNext()) {
+            final String key = keyIterator.next();
+
+            // Last one?
+            if (!keyIterator.hasNext())
+                break;
+
+            // Get compound from array
+            // Allows the use of paths like Items[0].id
+            else if (isArray(key)) {
+                String arrKey = removeArrStatement(key);
+                if (!compound.hasKey(arrKey)) return null;
+                compound = compound.getCompoundList(arrKey).get(getArrayIndex(key));
+            }
+
+            // Get compound from key
+            else {
+                if (!compound.hasKey(key)) return null;
+                compound = compound.getCompound(key);
+            }
+        }
+
+        return compound;
+    }
+
+    private static final Set<NBTType> SIMPLE_TYPES =  new HashSet<>(Arrays.asList(
+            NBTType.NBTTagByte, NBTType.NBTTagDouble, NBTType.NBTTagFloat, NBTType.NBTTagInt,
+            NBTType.NBTTagLong, NBTType.NBTTagShort, NBTType.NBTTagString
+    ));
+    private boolean anyValueMatches(String lastKey, List<Object> values, final NBTCompound nbtCompound) {
+        // Can only expect to compare a value if it's a primitive/String
+        if (!SIMPLE_TYPES.contains(nbtCompound.getType(lastKey))) return false;
+
+        for (Object o : values) {
+            if (equals(getSimpleDataFromCompound(nbtCompound, lastKey), o))
+                return true;
+        }
+
+        return false;
+    }
+
+    private boolean isArray(final String key) {
+        return key.endsWith("]");
+    }
+
+    private String removeArrStatement(final String key) {
+        return key.replaceAll("\\[\\d*]$", "");
+    }
+
+    private int getArrayIndex(final String key) {
+        return Integer.parseInt(key.replaceAll(".+\\[|]$", ""));
+    }
+
+    private Object getSimpleDataFromCompound(final NBTCompound compound, String key) {
+        NBTType type = compound.getType(key);
+
+        if (type == NBTType.NBTTagInt) return compound.getInteger(key);
+        else if (type == NBTType.NBTTagShort) return compound.getShort(key);
+        else if (type == NBTType.NBTTagLong) return compound.getLong(key);
+        else if (type == NBTType.NBTTagFloat) return compound.getFloat(key);
+        else if (type == NBTType.NBTTagDouble) return compound.getDouble(key);
+        else if (type == NBTType.NBTTagByte) return compound.getByte(key);
+        else return  compound.getString(key);
+    }
+
+    private boolean equals(Object simpleData, Object expected) {
+        if (simpleData instanceof Number) {
+            if (!(expected instanceof Number)) return false;
+            Double sD = ((Number) simpleData).doubleValue();
+            Double eD = ((Number) expected).doubleValue();
+
+            return eD.compareTo(sD) == 0;
+        }
+        else if (simpleData instanceof String) {
+            if (!(expected instanceof String)) return false;
+
+            return ((String) simpleData).equals((expected));
+        }
+
+
         return false;
     }
 }
